197ffe97eae9e1317dfa2d63529d6b9e7abeec3c
Paul Tagliamonte <paultag@debian.org>
2012-11-16 22:09:30 -0500

    404 page

--- app.py
+++ app.py
@@ -12,29 +12,6 @@ from monomoy.core import db
 app = Flask(__name__)
 
 
-@app.template_filter('md5hash')
-def _hash_email(obj):
-    return hashlib.md5(obj).hexdigest()
-
-
-@app.template_filter('humanize_date')
-def _humanize_date_filter(obj):
-    return naturalday(obj)
-
-
-@app.template_filter('humanize_size')
-def _humanize_size_filter(obj):
-    return naturalsize(obj)
-
-
-@app.template_filter('display_name')
-def _display_name_filter(obj):
-    user = db.users.find_one({"_id": obj})
-    if user is None:
-        user = db.users.find_one({"_id": ObjectId(obj)})
-    return "{first_name} {last_name}".format(**user)
-
-
 @app.route("/")
 def about():
     return render_template('about.html', **{})
@@ -82,6 +59,40 @@ def user(user_id):
         "user": user,
         "packages": db.packages.find({"user": user['_id']})
     })
+
+# Error pages, etc.
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+# Template filters, etc.
+
+
+@app.template_filter('md5hash')
+def _hash_email(obj):
+    return hashlib.md5(obj).hexdigest()
+
+
+@app.template_filter('humanize_date')
+def _humanize_date_filter(obj):
+    return naturalday(obj)
+
+
+@app.template_filter('humanize_size')
+def _humanize_size_filter(obj):
+    return naturalsize(obj)
+
+
+@app.template_filter('display_name')
+def _display_name_filter(obj):
+    user = db.users.find_one({"_id": obj})
+    if user is None:
+        user = db.users.find_one({"_id": ObjectId(obj)})
+    return "{first_name} {last_name}".format(**user)
+
 
 if __name__ == "__main__":
     app.debug = True
