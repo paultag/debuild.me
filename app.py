f8cec6bbad4fb186467dfe26e8b86f6f932d5a34
Paul Tagliamonte <paultag@debian.org>
2012-11-17 23:23:54 -0500

    foo

--- app.py
+++ app.py
@@ -60,6 +60,21 @@ def check(check_id):
     })
 
 
+@app.route("/job/<job_id>")
+def job(job_id):
+    job = db.jobs.find_one({"_id": job_id})
+    if job is None:
+        job = db.jobs.find_one({"_id": ObjectId(job_id)})
+    if job is None:
+        abort(404)
+
+    # XXX: Refactor all this garbage.
+
+    return render_template('job.html', **{
+        "job": job,
+        "checks": db.checks.find({"job": job['_id']})
+    })
+
 
 @app.route("/user/<user_id>")
 def user(user_id):
