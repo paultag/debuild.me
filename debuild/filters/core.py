d55f0b942b2299ce84023eb6f623d7f6698021c2
Paul Tagliamonte <paultag@debian.org>
2012-11-25 09:35:15 -0500

    adding in sane display

--- debuild/filters/core.py
+++ debuild/filters/core.py
@@ -26,3 +26,23 @@ def _display_name_filter(obj):
     if user is None:
         user = db.users.find_one({"_id": ObjectId(obj)})
     return "{first_name} {last_name}".format(**user)
+
+
+@app.template_filter('job_name')
+def _job_name_filter(obj):
+    job = db.jobs.find_one({"_id": obj})
+    if job is None:
+        job = db.job.find_one({"_id": ObjectId(obj)})
+    if job is None:
+        return obj
+    return "{type}".format(**job)
+
+
+@app.template_filter('package_name')
+def _package_name_filter(obj):
+    package = db.packages.find_one({"_id": obj})
+    if package is None:
+        package = db.packages.find_one({"_id": ObjectId(obj)})
+    if package is None:
+        return obj
+    return "{Source}/{Version}".format(**package['changes'])
