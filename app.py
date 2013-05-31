8a7ce5c1e6e60fcd96f7df51a6492ac3f807bf43
Paul Tagliamonte <tag@pault.ag>
2013-05-30 21:34:59 -0400

    update

--- app.py
+++ app.py
@@ -20,15 +20,11 @@
 
 from flask import Flask
 from debuild.blueprints.frontend import frontend
-from debuild.blueprints.apiv1 import api as apiv1
-from debuild.blueprints.uiapiv1 import uiapi as uiapiv1
 
 
 app = Flask(__name__)
 
 app.register_blueprint(frontend)
-app.register_blueprint(apiv1, url_prefix='/api/v1')
-app.register_blueprint(uiapiv1, url_prefix='/uiapi/v1')
 
 
 if __name__ == '__main__':
